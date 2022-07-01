d6efc6b60e276b556ab0edec643b001cc70da190
Alan Poveda <alsepoveda@gmail.com>
2022-06-30 22:27:26 -0300

    Add get-user(id) and get_user_by(params) functions to find a user to list

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -12,6 +12,13 @@ defmodule Rumbl.Accounts do
       %User{id: 2, name: "Natalia", username: "nataliahieda"},
       %User{id: 3, name: "Gabriel", username: "gabrielmelo"}
     ]
+  end
 
+  def get_user(id) do
+    Enum.find(list_users(), &(&1.id == id))
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), &(Enum.all?(params, fn {key, val} -> Map.get(&1, key) == val end)))
   end
 end
