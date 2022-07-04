f7aeb0e54d5e17b22d040619fd829b4b39784f7e
Alan Poveda <alsepoveda@gmail.com>
2022-07-04 18:49:48 -0300

    Add list_users, get_user, get_user_by usin a Repo, api for Ecto and using database

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,24 +1,34 @@
 defmodule Rumbl.Accounts do
   @moduledoc """
-  The Accounts context
+  The Accounts context using Repo API
 
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users() do
-    [
-      %User{id: 1, name: "Alan Poveda", username: "alanpoveda"},
-      %User{id: 2, name: "Natalia Silva", username: "nataliahieda"},
-      %User{id: 3, name: "Gabriel Melo", username: "gabrielmelo"}
-    ]
+    Repo.all(User)
+    # [
+    #   %User{id: 1, name: "Alan Poveda", username: "alanpoveda"},
+    #   %User{id: 2, name: "Natalia Silva", username: "nataliahieda"},
+    #   %User{id: 3, name: "Gabriel Melo", username: "gabrielmelo"}
+    # ]
   end
 
   def get_user(id) do
-    Enum.find(list_users(), &(&1.id == id))
+    Repo.get(User, id)
+    # Enum.find(list_users(), &(&1.id == id))
+  end
+
+  @doc """
+  Return a error if not found a user
+  """
+  def get_user!(id) do
+    Repo.get(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), &(Enum.all?(params, fn {key, val} -> Map.get(&1, key) == val end)))
+    Repo.get_by(User, params)
+    # Enum.find(list_users(), &(Enum.all?(params, fn {key, val} -> Map.get(&1, key) == val end)))
   end
 end
